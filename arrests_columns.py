e3113155dd9ea4dcba46ab4b40f4c74157a379d5
Kevin Prehn <kprehn01@gmail.com>
2025-11-01 13:37:27 -0400

    arrests and detentions cleaning scripts

--- arrests_columns.py
+++ arrests_columns.py
@@ -1,26 +1,36 @@
-from enum import Enum
+from tables import Tables
+from enum import Enum, auto
 
 class ArrestsColumns(str, Enum):
-    APPREHENSION_DATE = "Apprehension Date"  # date
-    APPREHENSION_STATE = "Apprehension State"  # categorical
-    # APPREHENSION_COUNTY = "Apprehension County"  # (entirely missing)
-    APPREHENSION_AOR = "Apprehension AOR"  # categorical. AOR: "Area of Responsibility"
-    FINAL_PROGRAM = "Final Program"  # categorical. e.g., "ERO Criminal Area Program"
-    # FINAL_PROGRAM_GROUP = "Final Program Group"  # categorial. e.g., ICE
-    APPREHENSION_METHOD = "Apprehension Method"  # categorical 
-    APPREHENSION_CRIMINALITY = "Apprehension Criminality"
-    CASE_STATUS = "Case Status"
-    CASE_CATEGORY = "Case Category"
-    DEPARTED_DATE = "Departed Date"
-    DEPARTURE_COUNTRY = "Departure Country"
-    FINAL_ORDER_YES_NO = "Final Order Yes No"  # boolean
-    FINAL_ORDER_DATE = "Final Order Date"
-    # BIRTH_DATE = "Birth Date"  # redacted
-    BIRTH_YEAR = "Birth Year"
-    CITIZENSHIP_COUNTRY = "Citizenship Country"
-    GENDER = "Gender"  # boolean
-    APPREHENSION_SITE_LANDMARK = "Apprehension Site Landmark"
-    # ALIEN_FILE_NUMBER = "Alien File Number"  # redacted
-    # EID_CASE_ID = "EID Case ID"  # redacted
-    # EID_SUBJECT_ID = "EID Subject ID"  # redacted
-    UNIQUE_IDENTIFIER = "Unique Identifier"
+    APPREHENSION_DATE = ("Apprehension Date", "", False, False)
+    APPREHENSION_STATE = ("Apprehension State", "", False, False)  
+    APPREHENSION_COUNTY = ("Apprehension County", "", False, True) 
+    APPREHENSION_AOR = ("Apprehension AOR", "AOR = area of responsibility", False, False)
+    FINAL_PROGRAM = ("Final Program", "", False, False)
+    FINAL_PROGRAM_GROUP = ("Final Program Group", "Single value column", False, True)
+    APPREHENSION_METHOD = ("Apprehension Method", "", False, False)
+    APPREHENSION_CRIMINALITY = ("Apprehension Criminality", "", False, False)
+    CASE_STATUS = ("Case Status", "", False, False)
+    CASE_CATEGORY = ("Case Category", "", False, False)
+    DEPARTED_DATE = ("Departed Date", "", False, False)
+    DEPARTURE_COUNTRY = ("Departure Country", "", False, False)
+    FINAL_ORDER_YES_NO = ("Final Order Yes No", "YES/NO depending on whether final order date is defined", False, True)
+    FINAL_ORDER_DATE = ("Final Order Date", "date of removal order or a non-appealed order", False, False)
+    BIRTH_DATE = ("Birth Date", "", True, False)
+    BIRTH_YEAR = ("Birth Year", "", False, False)
+    CITIZENSHIP_COUNTRY = ("Citizenship Country", "", False, False)
+    GENDER = ("Gender", "", False, False)
+    APPREHENSION_SITE_LANDMARK = ("Apprehension Site Landmark", "", False, False)
+    ALIEN_FILE_NUMBER = ("Alien File Number", "Anonymized", True, False)
+    EID_CASE_ID = ("EID Case ID", "Anonymized", True, False)
+    EID_SUBJECT_ID = ("EID Subject ID", "Anonymized", True, False)
+    UNIQUE_IDENTIFIER = ("Unique Identifier", "derived from alien registration number", False, False)
+
+    def __init__(self, column_name, table, description, redacted, bad_data):
+        self.column_name = column_name
+        # self.table = table
+        self.description = description
+        self.redacted = redacted
+        self.bad_data = bad_data
+        self.useable = not redacted and not bad_data
+        
