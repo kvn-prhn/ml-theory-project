1196f86b73e280edcf9126779248cdd8f9712473
hstewart38 <hstewart38@gatech.edu>
2025-11-22 21:32:14 -0500

    Drop days from start > 600 in random forest, update analysis

--- encounters_random_forest_analyze.py
+++ encounters_random_forest_analyze.py
@@ -1,14 +1,47 @@
 from utils import log
 
+input_model_path = './out/encounters_random_forest_model_2.joblib'
+input_X_test_path = './out/encounters_random_forest_X_test.joblib'
+input_y_test_path = './out/encounters_random_forest_y_test.joblib'
+
 log("importing libraries")
 import joblib
+import numpy as np
 
 log("loading model")
-model = joblib.load('./out/encounters_random_forest_model_1.joblib')
+model = joblib.load(input_model_path)
 
 log("loading test data")
-X_test = joblib.load('./out/encounters_random_forest_X_test.joblib')
-y_test = joblib.load('./out/encounters_random_forest_y_test.joblib')
+X_test = joblib.load(input_X_test_path)
+y_test = joblib.load(input_y_test_path)
 
 log(f"Test accuracy: {model.score(X_test, y_test):.4f}")
 
+log("Overall Feature GINI Scores")
+importances = model.feature_importances_
+sorted_indices = np.argsort(importances)[::-1]
+for i in sorted_indices:
+    if (not importances[i] == 0):
+        print(f"{model.feature_names_in_[i]}: {importances[i]:.4f}")
+
+original_features = ['Num Encounters', 'Responsible AOR', 'Event Type', 'Final Program', 'Encounter Criminality', 'Birth Year', 'Citizenship Country', 'Gender', 'Days After Start']
+grouped_importances = {feature: 0.0 for feature in original_features}
+
+for i in range(len(model.feature_importances_)):
+    feature_name_one_hot = model.feature_names_in_[i]
+    feature_importance = model.feature_importances_[i]
+
+    matched = False
+    for original_feature in original_features:
+        if feature_name_one_hot.startswith(original_feature):
+            grouped_importances[original_feature] += feature_importance
+            matched = True
+            break
+    
+    if (not matched):
+        raise Exception("feature not found: " + feature_name_one_hot)
+
+log("Normalized Gini Importaces")
+sorted_features = sorted(grouped_importances.items(), key=lambda x: x[1], reverse=True)
+for feature, importance in sorted_features:
+    log(f"{feature}: {importance:.4f}")
--- encounters_random_forest_train.py
+++ encounters_random_forest_train.py
@@ -6,12 +6,13 @@ RUN #1: Baseline
 - Training accuracy: 0.9985
 - Test accuracy: 0.8688
 
-RUN #2: Remove last ??? months of data
-
-todo:
-- remove last few months of data
-- find good way to display the results for a powerpoint slide
+RUN #2: Only keep first 600 days of data
 """
+
+intput_data_path = 'ice_data/clean_data/encounters.parquet'
+output_model_path = './out/encounters_random_forest_model_2.joblib'
+output_X_test_path = './out/encounters_random_forest_X_test.joblib'
+output_y_test_path = './out/encounters_random_forest_y_test.joblib'
 
 from utils import log
 
@@ -21,14 +22,16 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import clean_encounters
 import joblib
-import os
 
 
 log("loading clean data")
-df = pd.read_parquet('ice_data/clean_data/encounters.parquet')
+df = pd.read_parquet(intput_data_path)
 
 log("combining rows with the same individual")
 clean_encounters.combine_duplicate_ids(df)
+
+log("drop 'Days After Start' > 600")
+df.drop(df[df['Days After Start'] > 600].index, inplace=True)
 
 features = ['Num Encounters', 'Responsible AOR', 'Event Type', 'Final Program', 'Encounter Criminality', 'Birth Year', 'Citizenship Country', 'Gender', 'Days After Start']
 target = 'Deported'
@@ -50,11 +53,10 @@ log(f"Training accuracy: {model.score(X_train, y_train):.4f}")
 log(f"Test accuracy: {model.score(X_test, y_test):.4f}")
 
 log("storing model")
-joblib.dump(model, './out/encounters_random_forest_model_1.joblib')
+joblib.dump(model, output_model_path)
 
 log("storing test data")
-X_test_path = './out/encounters_random_forest_X_test.joblib'
-y_test_path = './out/encounters_random_forest_y_test.joblib'
-if not os.path.exists(X_test_path):
-    joblib.dump(X_test, X_test_path)
-    joblib.dump(y_test, y_test_path)
+joblib.dump(X_test, output_X_test_path)
+joblib.dump(y_test, output_y_test_path)
+
+print(model.feature_importances_)
